5798ea18f7ec7a47f1bc57c1c1d33a2d3aa4abf3
riskycheng <riskycheng@gmail.com>
2022-09-03 12:24:15 +0800

    update the nanodet code to make the results rendering works

--- app/src/main/cpp/nanodetPlus/nativeNanodet.cpp
+++ app/src/main/cpp/nanodetPlus/nativeNanodet.cpp
@@ -11,7 +11,7 @@
 #include <sstream>
 #include <android/bitmap.h>
 #include "nanodet.h"
-
+#include "classNamesColors.h"
 using namespace cv;
 #define  LOG_TAG    "Jian_nanoDet_JNI"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
@@ -80,115 +80,8 @@ int resize_uniform(cv::Mat &src, cv::Mat &dst, cv::Size dst_size, object_rect &e
     return 0;
 }
 
-static const char *class_names[] = {"person", "bicycle", "car", "motorcycle", "airplane", "bus",
-                                    "train", "truck", "boat", "traffic light", "fire hydrant",
-                                    "stop sign", "parking meter", "bench", "bird", "cat", "dog",
-                                    "horse", "sheep", "cow", "elephant", "bear", "zebra",
-                                    "giraffe",
-                                    "backpack", "umbrella", "handbag", "tie", "suitcase",
-                                    "frisbee",
-                                    "skis", "snowboard", "sports ball", "kite", "baseball bat",
-                                    "baseball glove", "skateboard", "surfboard",
-                                    "tennis racket",
-                                    "bottle", "wine glass", "cup", "fork", "knife", "spoon",
-                                    "bowl",
-                                    "banana", "apple", "sandwich", "orange", "broccoli",
-                                    "carrot",
-                                    "hot dog", "pizza", "donut", "cake", "chair", "couch",
-                                    "potted plant", "bed", "dining table", "toilet", "tv",
-                                    "laptop",
-                                    "mouse", "remote", "keyboard", "cell phone", "microwave",
-                                    "oven",
-                                    "toaster", "sink", "refrigerator", "book", "clock", "vase",
-                                    "scissors", "teddy bear", "hair drier", "toothbrush"
-};
-const int color_list[80][3] =
-        {
-                //{255 ,255 ,255}, //bg
-                {216, 82,  24},
-                {236, 176, 31},
-                {125, 46,  141},
-                {118, 171, 47},
-                {76,  189, 237},
-                {238, 19,  46},
-                {76,  76,  76},
-                {153, 153, 153},
-                {255, 0,   0},
-                {255, 127, 0},
-                {190, 190, 0},
-                {0,   255, 0},
-                {0,   0,   255},
-                {170, 0,   255},
-                {84,  84,  0},
-                {84,  170, 0},
-                {84,  255, 0},
-                {170, 84,  0},
-                {170, 170, 0},
-                {170, 255, 0},
-                {255, 84,  0},
-                {255, 170, 0},
-                {255, 255, 0},
-                {0,   84,  127},
-                {0,   170, 127},
-                {0,   255, 127},
-                {84,  0,   127},
-                {84,  84,  127},
-                {84,  170, 127},
-                {84,  255, 127},
-                {170, 0,   127},
-                {170, 84,  127},
-                {170, 170, 127},
-                {170, 255, 127},
-                {255, 0,   127},
-                {255, 84,  127},
-                {255, 170, 127},
-                {255, 255, 127},
-                {0,   84,  255},
-                {0,   170, 255},
-                {0,   255, 255},
-                {84,  0,   255},
-                {84,  84,  255},
-                {84,  170, 255},
-                {84,  255, 255},
-                {170, 0,   255},
-                {170, 84,  255},
-                {170, 170, 255},
-                {170, 255, 255},
-                {255, 0,   255},
-                {255, 84,  255},
-                {255, 170, 255},
-                {42,  0,   0},
-                {84,  0,   0},
-                {127, 0,   0},
-                {170, 0,   0},
-                {212, 0,   0},
-                {255, 0,   0},
-                {0,   42,  0},
-                {0,   84,  0},
-                {0,   127, 0},
-                {0,   170, 0},
-                {0,   212, 0},
-                {0,   255, 0},
-                {0,   0,   42},
-                {0,   0,   84},
-                {0,   0,   127},
-                {0,   0,   170},
-                {0,   0,   212},
-                {0,   0,   255},
-                {0,   0,   0},
-                {36,  36,  36},
-                {72,  72,  72},
-                {109, 109, 109},
-                {145, 145, 145},
-                {182, 182, 182},
-                {218, 218, 218},
-                {0,   113, 188},
-                {80,  182, 188},
-                {127, 127, 0},
-        };
 
-void draw_bboxes(const cv::Mat &bgr, const std::vector<BoxInfo> &bboxes, object_rect effect_roi) {
-    cv::Mat image = bgr.clone();
+void draw_bboxes(const cv::Mat &image, const std::vector<BoxInfo> &bboxes, object_rect effect_roi) {
     int src_w = image.cols;
     int src_h = image.rows;
     int dst_w = effect_roi.width;
@@ -196,36 +89,30 @@ void draw_bboxes(const cv::Mat &bgr, const std::vector<BoxInfo> &bboxes, object_
     float width_ratio = (float) src_w / (float) dst_w;
     float height_ratio = (float) src_h / (float) dst_h;
 
+    for (const auto & box : bboxes) {
+        cv::Scalar color = cv::Scalar(color_list[box.label][0], color_list[box.label][1],
+                                      color_list[box.label][2]);
 
-    for (size_t i = 0; i < bboxes.size(); i++) {
-        const BoxInfo &bbox = bboxes[i];
-        cv::Scalar color = cv::Scalar(color_list[bbox.label][0], color_list[bbox.label][1],
-                                      color_list[bbox.label][2]);
-
-        cv::rectangle(image, cv::Rect(cv::Point((bbox.x1 - effect_roi.x) * width_ratio,
-                                                (bbox.y1 - effect_roi.y) * height_ratio),
-                                      cv::Point((bbox.x2 - effect_roi.x) * width_ratio,
-                                                (bbox.y2 - effect_roi.y) * height_ratio)), color);
+        cv::rectangle(image, cv::Rect(cv::Point(int((box.x1 - (float)effect_roi.x) * width_ratio),
+                                                int((box.y1 - (float)effect_roi.y) * height_ratio)),
+                                      cv::Point(int((box.x2 - (float)effect_roi.x) * width_ratio),
+                                                    int((box.y2 - (float)effect_roi.y) * height_ratio))), color);
 
         char text[256];
-        sprintf(text, "%s %.1f%%", class_names[bbox.label], bbox.score * 100);
+        sprintf(text, "%s %.1f%%", class_names[box.label], box.score * 100);
 
         int baseLine = 0;
         cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.4, 1, &baseLine);
 
-        int x = (bbox.x1 - effect_roi.x) * width_ratio;
-        int y = (bbox.y1 - effect_roi.y) * height_ratio - label_size.height - baseLine;
+        int x = (int)((box.x1 - (float)effect_roi.x) * width_ratio);
+        int y = (int)((box.y1 - (float)effect_roi.y) * height_ratio) - label_size.height - baseLine;
         if (y < 0)
             y = 0;
         if (x + label_size.width > image.cols)
             x = image.cols - label_size.width;
 
-        cv::rectangle(image, cv::Rect(cv::Point(x, y),
-                                      cv::Size(label_size.width, label_size.height + baseLine)),
-                      color, -1);
-
         cv::putText(image, text, cv::Point(x, y + label_size.height),
-                    cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(255, 255, 255));
+                    cv::FONT_HERSHEY_SIMPLEX, 0.4, color);
     }
 }
 
@@ -254,34 +141,24 @@ Java_com_fatfish_chengjian_utils_JNIManager_nanoDet_1Detect(JNIEnv *env, jobject
     auto *imagePtr = (uint8_t *) _inputBitmap;
 
     //get image info
-    int width = bmapInfo.width;
-    int height = bmapInfo.height;
-    int format = bmapInfo.format;
+    auto width = bmapInfo.width;
+    auto height = bmapInfo.height;
 
     int model_height = NanoDet::detector->input_size[0];
     int model_width = NanoDet::detector->input_size[1];
 
-    LOGI("Jian >>> model-w:%d, h:%d", model_width, model_height);
-    Mat image = Mat(height, width, CV_8UC4);
+    Mat image = Mat((int)height, (int)width, CV_8UC4);
     image.data = imagePtr;
 
     Mat tmpMat;
     cvtColor(image, tmpMat, COLOR_RGBA2BGR);
 
-    LOGI("Jian >>> 2");
-    object_rect effect_roi;
+    object_rect effect_roi{};
     cv::Mat resized_img;
     resize_uniform(tmpMat, resized_img, cv::Size(model_width, model_height), effect_roi);
-    LOGI("Jian >>> 3");
     auto results = NanoDet::detector->detect(resized_img, 0.4, 0.5);
 
-    LOGI("Jian detected objects >>> %lu", results.size());
-    for (auto item : results)
-    {
-        LOGI("item:%s, conf:%.2f, x1:%f, y1:%f, x2:%f, y2:%f", class_names[item.label], item.score, item.x1, item.y1, item.x2, item.y2);
-    }
     draw_bboxes(image, results, effect_roi);
-    rectangle(image, Point(100, 100), Point(200, 200), Scalar(255, 0, 0));
     AndroidBitmap_unlockPixels(env, inputBitmap);
     tmpMat.release();
     LOGI("exiting %s", __FUNCTION__);
